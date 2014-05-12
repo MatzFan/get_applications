ef25e1e8d65791e5de852f557abd83268d6eb9e0
Bruce Steedman <bruce.steedman@gmail.com>
2014-05-12 17:52:26 +0100

    Split curl command into substrings so variables can be used in params

--- web.rb
+++ web.rb
@@ -1,14 +1,21 @@
 require 'sinatra'
 require 'json'
 
-delim = "href=https://www.mygov.je//Planning/Pages/PlanningApplicationDetail.aspx?s=1&amp;r="
-apps_json =`curl -s -X POST -H "Content-Type: application/json" -d '{"URL":"https://www.mygov.je//Planning/Pages/Planning.aspx","CommonParameters":"|05|1||||","SearchParameters":"|1301||||0|All|All|8|2|2014|8|5|2014"}' https://www.mygov.je/_layouts/PlanningAjaxServices/PlanningSearch.svc/Search`
-my_hash = JSON.parse(apps_json)
-array = my_hash['MapMarkerArray']
-apps_array = array.join('').split('href=https://www.mygov.je//Planning/Pages/PlanningApplicationDetail.aspx?s=1&amp;r=')
-app_nums = apps_array[1..10].map { |app| app.split('>')[0].split('/')[2].to_i }
-latest_app_num = app_nums.sort.reverse.first.to_s
+DELIM = 'href=https://www.mygov.je//Planning/Pages/PlanningApplicationDetail.aspx?s=1&amp;r='
+CURL = 'curl -s -X POST -H "Content-Type: application/json" -d '
+PARAMS = '{"URL":"https://www.mygov.je//Planning/Pages/Planning.aspx","CommonParameters":"|05|1||||","SearchParameters":"|1301||||0|All|All|8|2|2014|8|5|2014"}'
+URL = 'https://www.mygov.je/_layouts/PlanningAjaxServices/PlanningSearch.svc/Search'
+CURL_COMMAND = CURL + "'" + PARAMS + "' " + URL # note space before URL
+
+def get_latest_app_num
+  apps_json = `#{CURL_COMMAND}`
+  my_hash = JSON.parse(apps_json)
+  array = my_hash['MapMarkerArray']
+  apps_array = array.join('').split(DELIM)
+  app_nums = apps_array[1..10].map {|app| app.split('>')[0].split('/')[2].to_i}
+  latest_app_num = app_nums.sort.last.to_s
+end
 
 get '/' do
-  latest_app_num
+  get_latest_app_num
 end
