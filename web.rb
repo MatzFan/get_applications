f8fa13d86868ec34b91494e7f046105494fb709d
Bruce Steedman <bruce.steedman@gmail.com>
2014-08-18 21:21:27 +0100

    Add route num_apps_for a given year

--- web.rb
+++ web.rb
@@ -5,6 +5,11 @@ get '/' do
   Scraper.new(2014).latest_app_num
 end
 
+get '/num_apps_for' do
+  year = params[:year]
+  Scraper.new(year).num_apps.to_s
+end
+
 get '/app_refs_for' do
   year = params[:year]
   page_num = params[:page_num]
