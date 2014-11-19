3d2a96528a5550f285e9777a8d15176b0fb9002c
Bruce Steedman <bruce.steedman@gmail.com>
2014-11-19 20:54:17 +0000

    Change DB config to find Heroku root cert file

--- config/database.rb
+++ config/database.rb
@@ -1,11 +1,17 @@
 require 'openssl'
 
+if ENV['RACK_ENV'] == 'production'
+  cert_file = '/usr/lib/ssl/certs/ca-certificates.crt' # Heroku
+else
+  cert_file = OpenSSL::X509::DEFAULT_CERT_FILE
+end
+
 DB_CONFIG = {
   host: ENV['FMS_HOST'],
   account_name: ENV['FMS_ACCOUNT_NAME'],
   password: ENV['FMS_PASSWORD'],
   ssl: true,
   port: 443,
-  root_cert_name: File.basename(OpenSSL::X509::DEFAULT_CERT_FILE),
-  root_cert_path: File.dirname(OpenSSL::X509::DEFAULT_CERT_FILE)
+  root_cert_name: File.basename(cert_file),
+  root_cert_path: File.dirname(cert_file)
 }
